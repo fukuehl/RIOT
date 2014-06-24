901f16d72f9960d7c06f6b038a19ab95491a3668
epiktet <kuehling@zedat.fu-berlin.de>
2014-06-24 15:49:16 +0200

    Timer Changes, Prescaler probably wrong implemented

--- cpu/nrf51822qfaa/periph/timer.c
+++ cpu/nrf51822qfaa/periph/timer.c
@@ -28,10 +28,8 @@
 #include "nrf51.h"
 #include "nrf51_bitfields.h"
 
-
-
 typedef struct {
-    void (*cb)(int);
+	void (*cb)(int);
 } timer_conf_t;
 
 /**
@@ -45,113 +43,274 @@ typedef struct {
  */
 timer_conf_t config[TIMER_NUMOF];
 
+int timer_init(tim_t dev, unsigned int ticks_per_us, void (*callback)(int)) {
 
-
-int timer_init(tim_t dev, unsigned int ticks_per_us, void (*callback)(int))
-{
-
-    volatile NRF_TIMER_Type * p_timer;
-
-    // Start 16 MHz crystal oscillator.
-    NRF_CLOCK->EVENTS_HFCLKSTARTED  = 0;
-    NRF_CLOCK->TASKS_HFCLKSTART     = 1;
-
-    // Wait for the external oscillator to start up.
-    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
-    {
-        // Do nothing.
-    }
-
-    switch (dev) {
+	volatile NRF_TIMER_Type * p_timer;
+	switch (dev) {
 
 #if TIMER_0_EN
-    case (TIMER_0):
-		 p_timer = NRF_TIMER0;
+	case (TIMER_0):
+		p_timer = NRF_TIMER0;
 		break;
 #endif
 
 #if TIMER_1_EN
-    case (TIMER_1):
-		 p_timer = NRF_TIMER1;
+	case (TIMER_1):
+		p_timer = NRF_TIMER1;
 		break;
 #endif
 
 #if TIMER_2_EN
-    case (TIMER_2):
-		 p_timer = NRF_TIMER2;
+	case (TIMER_2):
+		p_timer = NRF_TIMER2;
 		break;
 #endif
 
-#if TIMER_3_EN
-    case (TIMER_3):
-		 p_timer = NRF_TIMER3;
+	}
+
+	/* save callback */
+	config[dev].cb = callback;
+
+	p_timer->BITMODE = TIMER_BITMODE_BITMODE_32Bit;	//32 Bit Mode
+	p_timer->MODE    = TIMER_MODE_MODE_Timer;       // Set the timer in Timer Mode.
+
+	switch (ticks_per_us) {
+		case 1:
+			p_timer->PRESCALER = 32;
+			break;
+		case 2:
+			p_timer->PRESCALER = 16;
+			break;
+		case 3:
+			p_timer->PRESCALER = 8;
+			break;
+		case 4:
+			p_timer->PRESCALER = 4;
+			break;
+		case 5:
+			p_timer->PRESCALER = 2;
+			break;
+
+		default:
+			break;
+	}
+
+
+	return 1;
+}
+
+int timer_set(tim_t dev, int channel, unsigned int timeout) {
+	volatile NRF_TIMER_Type * p_timer;
+
+	/* get timer base register address */
+	switch (dev) {
+
+#if TIMER_0_EN
+	case (TIMER_0):
+		p_timer = NRF_TIMER0;
 		break;
 #endif
 
+#if TIMER_1_EN
+	case (TIMER_1):
+		p_timer = NRF_TIMER1;
+		break;
+#endif
+
+#if TIMER_2_EN
+	case (TIMER_2):
+		p_timer = NRF_TIMER2;
+		break;
+#endif
+
+	}
+
+	/* set timeout value */
+
+	switch (channel) {
+		case 0:
+			p_timer->CC[0] = timeout;
+
+			break;
+		case 1:
+			p_timer->CC[1] = timeout;
+
+			break;
+		case 2:
+			p_timer->CC[2] = timeout;
+
+			break;
+		case 3:
+			p_timer->CC[3] = timeout;
+
+			break;
+		default:
+			break;
+	}
+
+	return 1;
+}
+
+int timer_clear(tim_t dev, int channel) {
+	volatile NRF_TIMER_Type * p_timer;
+
+	/* get timer base register address */
+	switch (dev) {
+
+#if TIMER_0_EN
+	case (TIMER_0):
+		p_timer = NRF_TIMER0;
+		break;
+#endif
+
+#if TIMER_1_EN
+	case (TIMER_1):
+		p_timer = NRF_TIMER1;
+		break;
+#endif
+
+#if TIMER_2_EN
+	case (TIMER_2):
+		p_timer = NRF_TIMER2;
+		break;
+#endif
+
+	}
+
+	/* set timeout value */
+
+	switch (channel) {
+		case 0:
+			p_timer->CC[0] = 0;
+
+			break;
+		case 1:
+			p_timer->CC[1] = 0;
+
+			break;
+		case 2:
+			p_timer->CC[2] = 0;
+
+			break;
+		case 3:
+			p_timer->CC[3] = 0;
+
+			break;
+		default:
+			break;
+	}
+
+	return 1;
+}
+
+unsigned int timer_read(tim_t dev) {
+    switch (dev) {
+#if TIMER_0_EN
+        case TIMER_0:
+            return NRF_TIMER0->CC[0];
+#endif
+#if TIMER_1_EN
+        case TIMER_1:
+        	return NRF_TIMER1->CC[0];
+#endif
+#if TIMER_2_EN
+        case TIMER_2:
+        	return NRF_TIMER2->CC[0];
+#endif
+        case TIMER_UNDEFINED:
+        default:
+            return 0;
     }
+}
 
-    //p_timer->
-
-
-    p_timer->MODE           = TIMER_MODE_MODE_Timer;        // Set the timer in Timer Mode.
-    p_timer->PRESCALER      = 9;                            // Prescaler 9 produces 31250 Hz timer frequency => 1 tick = 32 us.
-    p_timer->BITMODE        = TIMER_BITMODE_BITMODE_16Bit;  // 16 bit mode.
-    p_timer->TASKS_CLEAR    = 1;                            // clear the task first to be usable for later.
-
-    // With 32 us ticks, we need to multiply by 31.25 to get milliseconds.
-    //p_timer->CC[0]          = number_of_ms * 31;
-    //p_timer->CC[0]         += number_of_ms / 4;
-    p_timer->CC[0] 			= ticks_per_us;
-    p_timer->TASKS_START    = 1;                    // Start timer.
-
-    while (p_timer->EVENTS_COMPARE[0] == 0)
-    {
-        // Do nothing.
+void timer_start(tim_t dev) {
+    switch (dev) {
+#if TIMER_0_EN
+        case TIMER_0:
+            NRF_TIMER0->TASKS_START = 1;
+            break;
+#endif
+#if TIMER_1_EN
+        case TIMER_1:
+        	NRF_TIMER1->TASKS_START = 1;
+            break;
+#endif
+#if TIMER_2_EN
+        case TIMER_2:
+        	NRF_TIMER2->TASKS_START = 1;
+            break;
+#endif
+        case TIMER_UNDEFINED:
+            break;
     }
-
-    p_timer->EVENTS_COMPARE[0]  = 0;
-    p_timer->TASKS_STOP         = 1;                // Stop timer.
-
-
-    return -1;
 }
 
-int timer_set(tim_t dev, int channel, unsigned int timeout)
-{
-    return -1;
+void timer_stop(tim_t dev) {
+    switch (dev) {
+#if TIMER_0_EN
+        case TIMER_0:
+            NRF_TIMER0->TASKS_STOP = 1;
+            break;
+#endif
+#if TIMER_1_EN
+        case TIMER_1:
+        	NRF_TIMER1->TASKS_STOP = 1;
+            break;
+#endif
+#if TIMER_2_EN
+        case TIMER_2:
+        	NRF_TIMER2->TASKS_STOP = 1;
+            break;
+#endif
+        case TIMER_UNDEFINED:
+            break;
+    }
 }
 
-int timer_clear(tim_t dev, int channel)
-{
-    return -1;
+void timer_irq_enable(tim_t dev) {
+    switch (dev) {
+#if TIMER_0_EN
+        case TIMER_0:
+            NRF_TIMER0->INTENSET = 1;
+            break;
+#endif
+#if TIMER_1_EN
+        case TIMER_1:
+        	NRF_TIMER1->INTENSET = 1;
+            break;
+#endif
+#if TIMER_2_EN
+        case TIMER_2:
+        	NRF_TIMER2->INTENSET = 1;
+            break;
+#endif
+        case TIMER_UNDEFINED:
+            break;
+    }
 }
 
-unsigned int timer_read(tim_t dev)
-{
-    return 0;
+void timer_irq_disable(tim_t dev) {
+    switch (dev) {
+#if TIMER_0_EN
+        case TIMER_0:
+            NRF_TIMER0->INTENCLR = 1;
+            break;
+#endif
+#if TIMER_1_EN
+        case TIMER_1:
+        	NRF_TIMER1->INTENCLR = 1;
+            break;
+#endif
+#if TIMER_2_EN
+        case TIMER_2:
+        	NRF_TIMER2->INTENCLR = 1;
+            break;
+#endif
+        case TIMER_UNDEFINED:
+            break;
+    }
 }
 
-void timer_start(tim_t dev)
-{
-    /* TODO */
-}
-
-void timer_stop(tim_t dev)
-{
-    /* TODO */
-}
-
-void timer_irq_enable(tim_t dev)
-{
-    /* TODO */
-}
-
-void timer_irq_disable(tim_t dev)
-{
-    /* TODO */
-}
-
-void timer_reset(tim_t dev)
-{
-    /* TODO */
+void timer_reset(tim_t dev) {
+	/* TODO */
 }
